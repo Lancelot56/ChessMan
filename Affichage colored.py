1b2a431905239ad92ac05377908b09a42c42bcb5
Lancelot56 <gde81000@gmail.com>
2022-12-07 03:29:48 +0100

    first commit

---
+++ Affichage colored.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  2 07:01:36 2022
+
+@author: m
+"""
+
+from termcolor import colored
+
+print(colored('You need to type a four-digits composite number', 'green'))
---
+++ benvenido.py
@@ -0,0 +1,8 @@
+-*- coding: utf-8 -*-
+"""
+Created on Fri Sep  2 07:01:36 2022
+
+@author: m
+"""
+
+print('You need to type a four-digits composite number')
